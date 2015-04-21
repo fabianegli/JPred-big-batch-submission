bb96b8c14c7c00cf051ecfdcef4108d860cddb7d
fabianegli <fabian.egli@biol.ethz.ch>
2015-04-21 19:40:24 +0200

    Updated preamble with appropriate  copyright message.

--- multibatch_jpredapi.pl
+++ multibatch_jpredapi.pl
@@ -1,23 +1,28 @@
 #!/usr/bin/env perl 
 
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# multibatch_jpredapi.pl makes regulated batch submissions of jobs to JPred4 REST API.
-# Copyright (C) 2015 Fabian EGLI (fabian DOT egli AT biol DOT ethz DOT ch)
-# 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-# 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-# 
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#                                                                                       #
+#   multibatch_jpredapi.pl schedules job submissions to the  REST API of JPred4.        #
+#                                                                                       #
+#   Copyright (C) 2015                                                                  #
+#                                                                                       #
+#   ETH Zuerich                                                                         #
+#   Institute of Molecular Health Sciences                                              #
+#   Fabian EGLI <fabian.egli@biol.ethz.ch)                                              #
+#                                                                                       #
+#   This program is free software: you can redistribute it and/or modify                #
+#   it under the terms of the GNU General Public License as published by                #
+#   the Free Software Foundation, either version 3 of the License, or                   #
+#   (at your option) any later version.                                                 #
+#                                                                                       #
+#   This program is distributed in the hope that it will be useful,                     #
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of                      #
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                       #
+#   GNU General Public License for more details.                                        #
+#                                                                                       #
+#   You should have received a copy of the GNU General Public License                   #
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.               #
+#                                                                                       #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 use strict;
